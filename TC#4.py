303dade496f1563b53ae5c2ffaa8775a686ddf77
JuliaRafuse <W0471961@campus.nscc.ca>
2024-10-11 19:10:17 -0300

    Add files via upload
    finished

---
+++ TC#4.py
@@ -0,0 +1,116 @@
+#student Name: Julia
+
+#Program Title: Tech Check 4
+
+#discription: Grade Average Calculator
+
+def main( ): #<-- Don’t change this line! 
+
+#Write your code below. It must be indented! 
+
+    def printMessage():
+        print("Grade Point Average Calculator")
+        return
+    output = print
+
+    def PrintMesasage():
+        print("Valid letter grades that can be entered: A, B, C, D, F.\
+    Valid grade modifiers are +, - or nothing.")
+    output = PrintMesasage
+
+
+    def PrintMesasage():
+        print("All letter grades except F can include a + or - symbol.\
+    Calculated grade point value cannot exceed 4.0.")
+        return
+    output = PrintMesasage
+
+    def getMessage(modifier):
+        message= "Please enter a letter grade for PROG1700\
+    Please enter a modifier (+, - or nothing) : "
+        return message
+    def PrintMesasage(message):
+        print(message)
+        return 
+
+    def getMessage(letter):
+        message= "Please enter a letter grade for NETW1700: "
+        return message
+    def PrintMesasage(message):
+        print(message)
+        return
+
+    def getMessage(modifier):
+        message= "Please enter a modifier (+, - or nothing) : "
+        return message
+    def PrintMesasage(message):
+        print(message)
+        return
+
+    def getMessage(letter):
+        message= "Please enter a letter grade for OSYS1200: "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    def getMessage(modifier):
+        message= "Please enter a modifier (+, - or nothing) : "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    def getMessage(letter):
+        message= "Please enter a letter grade for WEBD1000: "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+        
+    def getMessage(modifier):
+        message= "Please enter a modifier (+, - or nothing) : "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    def getMessage(letter):
+        message= "Please enter a letter grade for COMM1700: "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+        
+    def getMessage(modifier):
+        message= "Please enter a modifier (+, - or nothing) : "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    def getMessage(letter):
+        message= "Please enter a letter grade for DBAS1007: "
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    def getMessage(modifier):
+        message= "Please enter a modifier (+, - or nothing) :+"
+        return message
+    def printMessage(message):
+        print(message)
+        return
+
+    output= getMessage
+    PrintMesasage(output)
+    output= printMessage
+    PrintMessage= output
+
+#your code ends on the line above 
+
+#do not change any of the codes below! 
+
+if __name__ == "__main__":
+    main()
